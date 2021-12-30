2ed460973453517bddb65995341967215697eca0
Cosmo Wang <cosmo.wang@outlook.com>
2021-12-29 17:56:11 -0800

    use env vars

--- bangumi_ratings_server/settings.py
+++ bangumi_ratings_server/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'bangumi_ratings_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "dbname",
-        "USER": "root",
-        "PASSWORD": "pass",
-        "HOST": "db",
-        "PORT": "3306",
+        'NAME': os.environ.get('DATABASE_NAME'),
+        "USER": os.environ.get('DATABASE_USER'),
+        "PASSWORD": os.environ.get('DATABASE_PASSWORD'),
+        "HOST": os.environ.get('DATABASE_HOST'),
+        "PORT": os.environ.get('DATABASE_PORT'),
     }
 }
 
