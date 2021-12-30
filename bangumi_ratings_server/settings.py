0a8f4bdea9dd1a9907ed92b6af469f4e8f3769b4
Cosmo Wang <cosmo.wang@outlook.com>
2021-12-29 19:23:49 -0800

    added allowed host

--- bangumi_ratings_server/settings.py
+++ bangumi_ratings_server/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.244.10.157']
-
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
